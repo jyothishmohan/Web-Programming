33424c9dd52a8577c6c7975b5b9ae2d74de3c0fb
jyothishmohan <156563567+jyothishmohan@users.noreply.github.com>
2024-12-16 16:09:57 +0530

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -11,4 +11,31 @@ if ($conn->connect_error) {
 } else {
     echo "Connected";
 }
+
+
+$sql = "DROP TABLE IF EXISTS MyGuests";
+$conn->query($sql);
+
+$sql = "CREATE TABLE MyGuests(
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests Created Successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$ins = "INSERT INTO MyGuests (firstname, lastname, email) VALUES ('Jyothish', 'A', 'abc@gmail.com')";
+if ($conn->query($ins) === TRUE) {
+    echo "Values inserted successfully";
+} else {
+    echo "Error inserting values: " . $conn->error;
+}
+
+$conn->close();
 ?>
